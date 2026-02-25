eaa4edb20991431aee01be52e6f3a68c6a510c45
Sabrina Ferrara <ferrara.sabrina10@gmail.com>
2026-02-25 16:15:37 +0100

    add esercizi

--- esercizi.py
+++ esercizi.py
@@ -12,3 +12,39 @@ nomi = ['Anna,' 'Ciccio', 'Francesca', 'Annibale']
 nuova_nomi ={nome: 'Lungo' if len(nome) > 5 else 'Corto' for nome in nomi}
 print(nuova_nomi)
 
+"""
+#scrivere una pipeline (4 funzioni) che
+- riceve una lista di stringhe numeriche (numeri come stringa)
+- li converte in interi gestendo gli errori 
+- restituisce solo i > 10
+- calcolo la somma (sum(list))
+quando usiam except dentro il for mettiamo pass
+"""
+
+#conversione da stringhe a numeri
+def converti(lista):
+    numeri = []
+    for n in lista:
+        try:
+            numeri.append(int(n))
+        except ValueError:
+            pass
+    return numeri
+
+def filtra(lista):
+    return [n for n in lista if n > 10]
+
+def somma(lista):
+    return sum(lista)
+
+def main():
+    dati = ['4', '12', '31', '1', '3', '21', 'trenta']
+
+    numeri = converti(dati)
+    filtrati = filtra(numeri)
+    risultato = somma(filtrati)
+
+    print("Somma:", risultato)
+
+main()
+
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("second commit")
+lista = ['giallo', 'verde', 'blu']
+for colore in lista:
+    print(colore)
+
+numeri =[10,20,30]
+nuovi_numeri =[]
+#operazione di trasformazione
+for numero in numeri:
+    nuovo = numero + 2  #qua trasformiamo la collection origiale (numeri)
+    nuovi_numeri.append(nuovo)
+print(nuovi_numeri)
+
+for i in range (len(numeri)):
+    numeri[i] = numeri [i] + 2 #cosi sto modificando la lista iniziale, non va bene
+print(nuovi_numeri)
+
+#operazione di filtro (es. voglio filtrare solo numeri pari)
+
+numeri =[1,2,3,4,5,6,7,8,9,10] #creo nuova colezone
+nuovi_numeri = []
+pari =[]
+for numero in numeri:
+    if numero % 2 == 0:
+        pari.append(numero)
+print(pari)
+
+#aggiungere numero sia vfiltrato che modificato
+risultato = []
+
+for numero in numeri:
+    if numero % 2 == 0:
+        risultato.append(numero * 10)
+print(risultato)0
+
